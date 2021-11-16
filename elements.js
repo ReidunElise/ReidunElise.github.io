d09f587703e7dca9f420f58378560353cc4a4e44
benjaminFoldoy <61045636+benjaminFoldoy@users.noreply.github.com>
2021-11-16 14:37:42 +0100

    2

--- elements.js
+++ elements.js
@@ -1,4 +1,5 @@
 var i = document.querySelector("#forsidebilde").clientHeight;
+document.getElementById("underforsidebilde").style.height = String(i - 1 * parseInt(window.scrollY)) + "px";
 
     window.addEventListener("scroll", () => {
         //document.getElementById("forsidebilde").style.marginTop = String(-1 * parseInt(window.scrollY)) + "px";
