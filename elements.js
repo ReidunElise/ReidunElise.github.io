86b6f0704026f981e81134933e343811bca4818b
benjaminFoldoy <61045636+benjaminFoldoy@users.noreply.github.com>
2021-11-16 16:08:24 +0100

    3

--- elements.js
+++ elements.js
@@ -1,7 +1,19 @@
-var i = document.querySelector("#forsidebilde").clientHeight;
-document.getElementById("underforsidebilde").style.height = String(i - 1 * parseInt(window.scrollY)) + "px";
 
-    window.addEventListener("scroll", () => {
-        //document.getElementById("forsidebilde").style.marginTop = String(-1 * parseInt(window.scrollY)) + "px";
-        document.getElementById("underforsidebilde").style.height = String(i - 1 * parseInt(window.scrollY)) + "px";
-    });
+function reload_sizes(){
+    var i = document.querySelector("#forsidebilde").clientHeight;
+    document.getElementById("frontimagecontainer").style.height = String(i) + "px";
+    document.getElementById("forsidebilde").style.marginTop = String(-i -1 * parseInt(window.scrollY)) + "px";
+    document.getElementById("reidunelise").style.marginTop = String(-i + i*0.3 -0.5 * parseInt(window.scrollY)) + "px";
+}
+
+window.addEventListener("scroll", () => {
+    reload_sizes();    
+});
+
+window.addEventListener("resize", () => {
+    reload_sizes();
+})
+
+window.addEventListener("load", () => {
+    reload_sizes();
+})
